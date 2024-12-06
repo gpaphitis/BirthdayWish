87a06ff9e9cae410c7e53886639991ebb87381ff
Giorgos Paphitis <gpaphi02@ucy.ac.cy>
2024-12-06 15:12:23 +0200

    Completed
    Message only missing

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 document.addEventListener("DOMContentLoaded", () =>
 {
-   document.querySelector('#box').addEventListener('click', () =>
+   document.querySelector('#content').addEventListener('click', () =>
    {
       if (document.querySelector('#top').classList.contains('open'));
       else {
          document.querySelector('#top').classList.add('open');
          document.querySelector('#wish').classList.remove('hidden');
-         // let delayInMilliseconds = 4000; //1 second
+         let delayInMilliseconds = 4000; //1 second
          // setTimeout(function ()
          // {
-         //    document.querySelector('#content-wish').classList.remove('hidden');
-         //    document.querySelector('#full-content').classList.add('grey-out');
+         //    document.querySelector('#content-wish').classList.remove('hidden-wish');
+         //    document.querySelector('#content-wish').classList.add('semi-hidden');
+         //    setTimeout(function (){
+         //       document.querySelector('#content-wish').classList.remove('semi-hidden');
+         //       document.querySelector('#full-content').classList.add('grey-out');
+         //    },1000)
          // }, delayInMilliseconds);
+         document.querySelector('#content-wish').classList.remove('hidden-wish');
+         document.querySelector('#content-wish').classList.add('semi-hidden');
+         setTimeout(function ()
+         {
+            document.querySelector('#content-wish').classList.remove('semi-hidden');
+            document.querySelector('#full-content').classList.add('grey-out');
+         }, delayInMilliseconds);
       }
    });
 });
